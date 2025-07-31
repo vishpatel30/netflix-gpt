229a0b2048b9a5d2a8c899d9e170dec6d2920d1c
vishpatel30 <vishsoni30@gmail.com>
2025-07-31 19:41:43 +0530

    Project set up with Tailwind Css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='text-2xl font-bold text-red-300'>Hello lets build</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
